9d0a8de65d2861fefe7e9ef973e14a0fd9cee75b
dmlimgo <dongmyeong.lim@gmail.com>
2019-07-09 11:33:00 +0900

    [DM] new page added

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import PostPage from './views/PostPage.vue'
 import PortfolioPage from './views/PortfolioPage.vue'
 import LoginPage from './views/LoginPage.vue'
 import PortfolioNewPage from './views/PortfolioNewPage.vue'
+import HomePage2 from './views/HomePage2.vue'
 
 Vue.use(Router)
 
@@ -36,6 +37,11 @@ export default new Router({
 			path: '/portfolio/new',
 			name: 'new',
 			component: PortfolioNewPage
+		},
+		{
+			path: '/pjt2',
+			name: 'pjt2',
+			component: HomePage2
 		},
   ]
 })
---
+++ src/views/HomePage2.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        newhome
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'pjt2',
+}
+</script>
