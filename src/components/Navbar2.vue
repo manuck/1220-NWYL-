75ec03527b9e21d31f1abc749f429100f4707d47
dmlimgo <dongmyeong.lim@gmail.com>
2019-07-10 17:20:00 +0900

    [DM] navbar & cardview work

--- src/components/Navbar2.vue
+++ src/components/Navbar2.vue
@@ -1,10 +1,44 @@
 <template>
-    <div>
+    <div class="header-wrapper">
+        <div class="header-flex">
+            <div class="left">
+                <div class="logo">
+                    <a href="">
+                        NWYL
+                    </a>
+                </div>
+            </div>
+            <div class="right">
+                <div class="gitlab">
+                    <a href="">
+                        GITLAB
+                    </a>
+                </div>
+                <div class="weather">
+                    <a href="">
+                        WEATHER
+                    </a>
+                </div>
+                <div class="login">
+                    <a href="">
+                        LOGIN   
+                    </a>
+                </div>
+                <div class="signin">
+                    <a href="">
+                        SIGNIN
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'navbar2',
+    name: 'Navbar2',
 }
 </script>
+<style>
+    @import '../assets/css/Navbar2.css';
+</style>
