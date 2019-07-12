34a5c1207213d6208805e2e9f1ffdcb86f970330
dmlimgo <dongmyeong.lim@gmail.com>
2019-07-12 10:35:50 +0900

    [DM] 포트폴리오 링크 UI 구현

---
+++ src/components/PortfolioViewBox.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card-wrapper">
+        <div class="card-title">
+            포트폴리오
+        </div>
+        <div class="card-bar"/>
+        <div class="card-content-wrapper">
+            <u>See our works</u>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "PortfolioViewBox",    
+}
+</script>
+<style>
+
+</style>
+
+
