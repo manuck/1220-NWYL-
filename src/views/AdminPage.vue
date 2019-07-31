81a7b9f990786858fb583477da1d042020d5cf55
dmlimgo <dongmyeong.lim@gmail.com>
2019-07-31 10:32:14 +0900

    [DM] Admin페이지 레이아웃 약간 수정

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -2,16 +2,29 @@
     <div>
         <div class="admin-topnav">
             <div class="admin-topnav-flex">
-                <div class="admin-topnav-left">
-                    관리자 페이지
-                </div>
                 <div class="admin-topnav-right">
                     관리자 정보
                 </div>
             </div>
         </div>
         <div class="admin-aside">
-
+            <div class="admin-aside-flex">
+                <div class="admin-aside-li">
+                    대시 보드
+                </div>
+                <!-- <div class="admin-aside-li">
+                    페이지 정보
+                </div>
+                <div class="admin-aside-li">
+                    회원 정보
+                </div>
+                <div class="admin-aside-li">
+                    메뉴 정보
+                </div>
+                <div class="admin-aside-li">
+                    페이지 기록
+                </div> -->
+            </div>
         </div>
         <div class="admin-content">
 
