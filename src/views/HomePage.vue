2b61d649da30fa64b32f157e2d58afc084e11674
dmlimgo <dongmyeong.lim@gmail.com>
2019-08-08 16:17:05 +0900

    [DM] homepage / 애니메이션 구성 완료 반응형 필요

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,31 +4,38 @@
         <div class="main-wrapper">
             <div class="body-wrapper">
                 <div class="section-1">
-                    <div class="section-1-title">
-                        <span style="font-size:100px; line-height:100px;">
-                            12<span style="color: #c8e19d;">15</span>
-                        </span>
-                    </div>
                     <div class="section-1-content">
+                        <div class="section-1-content-title">
+                            <span style="font-size:100px; line-height:100px;">
+                                12<span style="color: #c8e19d;">15</span>
+                            </span>
+                        </div>
                         <div class="section-1-content-menu">
                             <!-- for shadow -->
-                            <div v-for="i in 20" :key="i" :class="'ball'+i" style="box-shadow: 0 0 0 5px black; z-index: -1;"/>
-                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 760 350" style="enable-background:new 0 0 760 350;" xml:space="preserve">
-                                <path id="XMLID_3_" style="fill:#f7f7f7;stroke:#000000;stroke-miterlimit:10;" d="M380,14.6C85.4,14.6,40.4,8.7,31.6,169.5v5.5  c0,0,0,5.5,0,5.5C40.4,341.3,85.4,335.4,380,335.4s339.6,5.9,348.4-154.9c0,0,0-5.5,0-5.5v-5.5C719.6,8.7,674.6,14.6,380,14.6z"/>
+                            <div class="section-1-ball-container" style="z-index: -2;">
+                                <div v-for="i in 20" :key="i" :class="'ball'+i" style="box-shadow: 0 0 0 5px black; z-index: -1;"/>
+                            </div>
+                            <div class="section-1-ball-container" style="z-index: -1">
+                                <div v-for="i in 20" :key="i" :class="'ball'+i"/>
+                            </div>
+                            <svg version="1.1" x="0px" y="0px" viewBox="0 0 760 350" style="enable-background:new 0 0 760 350;">
+                                <path style="fill:#f7f7f7;" d="M380,14.6C85.4,14.6,40.4,8.7,31.6,169.5v5.5  c0,0,0,5.5,0,5.5C40.4,341.3,85.4,335.4,380,335.4s339.6,5.9,348.4-154.9c0,0,0-5.5,0-5.5v-5.5C719.6,8.7,674.6,14.6,380,14.6z"/>
                             </svg>
-                            <div v-for="i in 20" :key="i" :class="'ball'+i"/>
-
-                            <!-- <div id="main-korean" class="section-1-menu-box"></div> -->
-                            <!-- <div id="main-special" class="section-1-menu-box"></div> -->
-                            <!-- <div id="main-star" class="section-1-menu-box"></div> -->
+                            <div id="main-korean" class="section-1-menu-box"></div>
+                            <div id="main-special" class="section-1-menu-box"></div>
+                            <div id="main-star" class="section-1-menu-box"></div>
                         </div>
                         <SideNav/>
                     </div>
                     <div class="section-1-scroll">
-                        <div class="section-1-content-image">
+                        <div class="section-1-scroll-think">
+                            <div class="section-1-scroll-think-bubble bubble1"/>
+                            <div class="section-1-scroll-think-bubble bubble2"/>
+                        </div>
+                        <div class="section-1-content-image" style="z-index: -3;">
                             <img src="@/assets/images/lying_hd.png" style="height:100px; width:auto;" alt="main_image"/>
                         </div>
-                        <button class="scroll-round" v-on:click="pageScroll"/>
+                        <!-- <button class="scroll-round" v-on:click="pageScroll"/> -->
                     </div>
                 </div>
                 <div class="section-2">
@@ -102,6 +109,7 @@ export default {
             statY: 0,
             heightnow: 0,
             timer: null,
+            cloudtimer: null,
         }
     },
     created() {
@@ -117,15 +125,15 @@ export default {
         store.dispatch('addLog');
 
         var team_btn = document.querySelector('#team-icon')
-        console.log(team_btn)
         team_btn.addEventListener('click', this.pageScroll)
+        this.responsiveCloud()
+        window.addEventListener('resize', this.responsiveCloud)
     },
     methods: {
         autoMoveToSecond() {
             this.statY = window.scrollY;
             this.heightnow = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
             if(this.statY > 10 && this.statY < 100) {
-                console.log(this.heightnow)
                 window.scroll({
                     top: this.heightnow,
                     left: 0,
@@ -134,16 +142,47 @@ export default {
             }
         },
         pageScroll(e) {
-            console.log('pageScroll activated', this.timer)
+            // console.log('pageScroll activated', this.timer)
             if (!this.timer) {
                 let y = e.deltaY
                 let h = window.innerHeight
-                console.log('pageScroll if 문', this.timer, y, h)
+                // console.log('pageScroll if 문', this.timer, y, h)
                 if (y > 0 || !y)  window.scrollBy(0, h)
                 else        window.scrollBy(0, -h)
                 this.timer = setTimeout(() => {
                     this.timer = null
                 }, 500)
+            }
+        },
+        responsiveCloud() {
+            var container = document.getElementsByClassName('section-1-ball-container')
+            var svg = document.querySelector('svg')
+            var menubox = document.getElementsByClassName('section-1-menu-box')
+            var path = document.querySelector('path')
+            if (!this.timer) {
+                var height = parseInt(getComputedStyle(svg).height, 10)
+                var width = parseInt(getComputedStyle(svg).width, 10)
+                if ((width/760) < 1.4) {
+                    for (let i = 0; i < container.length; i++) {
+                        // container[i].style.height = height + "px"
+                        // container[i].style.width = width + "px"
+                        container[i].style.transform = 'scale(' + (width/760) + ')'
+                        // container[i].style.transform = 'scale(' + (width/760) + ',' + (height/350) + ')'
+                    }
+                } else {
+                    for (let i = 0; i < container.length; i++) {
+                        container[i].style.transform = 'scale(1.4)'
+                    }
+                }
+                console.log('here')
+                for (let i = 0; i < menubox.length; i++) {
+                    console.log('andhere')
+                    menubox[i].style.width = ((path.getBoundingClientRect().width / 3) - 80) + "px";
+                    menubox[i].style.height = path.getBoundingClientRect().height + "px";
+                }
+                this.timer = setTimeout(() => {
+                    this.timer = null
+                }, 50)
             }
         }
     },
